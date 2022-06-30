d567b29de4d62215c33d0637816a573e5a349b0a
keithwahhhhhhhh <idontknowaboutthis5@gmail.com>
2022-06-30 16:12:56 -0700

    Adding respawn, checkpoints, and health mechanics

--- Alebrije/Assets/Scripts/Health/Health.cs
+++ Alebrije/Assets/Scripts/Health/Health.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
-    private float currentHealth;
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private Behaviour[] components;
+    private bool invulnerable;
+    public float currentHealth { get; private set;}
+    private Animator anim;
+    private bool dead;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
     }
 
     public void TakeDamage(float _damage)
@@ -18,11 +25,48 @@ public class Health : MonoBehaviour
         
         if(currentHealth > 0 )
         {
-            
+            anim.SetTrigger("Hurt");
         }
         else {
+            if(!dead)
+            {
+                //foreach (Behaviour component in components)
+                    //component.enabled = false;
+                
+                anim.SetTrigger("Die");
+                //anim.SetBool("Grounded", true);
+                GetComponent<playerMovement>().enabled = false;
+                dead = true;
+            }
 
+        }
+       
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDamage(1);
         }
 
     }
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0,  startingHealth);
+    }
+
+    public void Respawn()
+    {
+        dead = false;
+        AddHealth(startingHealth);
+        anim.ResetTrigger("Die");
+        anim.Play("Idle");
+        GetComponent<playerMovement>().enabled = true;
+
+        //foreach (Behaviour component in components)
+            //component.enabled = true;
+    }
+
+
 }
+
