8faf53493ba95f6711230727cfa6511d73f5bec8
Chiedozie Ibe <70459771+charzar371@users.noreply.github.com>
2022-08-18 00:51:27 -0700

    Audio Manager

--- Alebrije/Assets/Scripts/Sound/SoundManager.cs
+++ Alebrije/Assets/Scripts/Sound/SoundManager.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class SoundManager : MonoBehaviour
 {
-    public static SoundManager instance { get; private set;}
+    public static SoundManager instance { get; private set; }
     private AudioSource source;
 
     private void Awake()
